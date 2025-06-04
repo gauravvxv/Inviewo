3d0b28df06580366a60e1c87f29aeaac6445e5bd
Gaurav Sharma <gauraxvx0410@gmail.com>
2025-06-04 14:10:10 +0530

    aaaa

--- my-app/app/contact/page.js
+++ my-app/app/contact/page.js
@@ -3,10 +3,10 @@ export default function Contact() {
     <div className="min-h-screen bg-blue-50 py-16 px-4">
       <div className="max-w-3xl mx-auto bg-white p-8 rounded-2xl shadow-md border border-blue-100">
         <a href="/">
-        <button className="mb-4 text-black underline hover:text-indigo-800">
-          ← Back to Home
-        </button>
-      </a>
+          <button className="mb-4 text-black underline hover:text-indigo-800">
+            ← Back to Home
+          </button>
+        </a>
         <h1 className="text-4xl font-bold text-blue-800 mb-4">Contact Us</h1>
 
         <p className="text-gray-700 mb-6">
@@ -19,6 +19,15 @@ export default function Contact() {
             <a href="mailto:hr@inviewo.com" className="text-blue-600 underline">
               hr@inviewo.com
             </a>
+          </li>
+          <li>
+            <strong>Phone:</strong>{' '}
+            <a href="tel:+919928425027" className="text-blue-600 underline">
+              +91-9928425027
+            </a>
+          </li>
+          <li>
+            <strong>Address:</strong> Aratt Felicita, Bangalore, Karnataka, India
           </li>
           <li>
             <strong>Business Hours:</strong> Monday – Saturday, 10:00 AM – 6:00 PM IST
