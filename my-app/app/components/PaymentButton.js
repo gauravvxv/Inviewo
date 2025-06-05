f72f2e1a3d0f7a0bc30337f753b0663d5e4bcd49
Gaurav Sharma <gauraxvx0410@gmail.com>
2025-06-05 14:13:55 +0530

    aaaa

--- my-app/app/components/PaymentButton.js
+++ my-app/app/components/PaymentButton.js
@@ -73,7 +73,7 @@ export default function PaymentButton({ sessionId, amount }) {
         contact: "",
       },
       theme: {
-        color: "#6366f1", // Tailwind indigo-500
+        color: "#6366f1", 
       },
     };
 
