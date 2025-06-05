8059de0903277c1f11f7f4c37ef13bcfebbd9420
Gaurav Sharma <gauraxvx0410@gmail.com>
2025-06-05 13:59:41 +0530

    aaaa

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -16,7 +16,6 @@ export default function HomePage() {
   "/logo/adobe.png"
 ];
 
-
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-purple-100 text-gray-900">
       <Navbar />
